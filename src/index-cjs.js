eb0b6ca28b76039f64e95b7fc8789b6f9df0b424
Charlike Mike Reagent <5038030+olstenlarck@users.noreply.github.com>
2017-10-22 20:32:33 +0300

    fix: add small note

--- src/index-cjs.js
+++ src/index-cjs.js
@@ -4,6 +4,10 @@
  */
 
 const isObject = require('isobject');
+
+// force ".js", because static analysis services/tools and other things
+// does not recognize, and practically there is no way even through their
+// configs to accomplish this. That's why we must stick to ".js" for now.
 const config = require('@std/esm')(module, { esm: 'js' })('./index-esm.js');
 
 const interop = (ex) => (isObject(ex) && 'default' in ex ? ex.default : ex);
