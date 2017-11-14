57fd6628b8ef4deee044ab1da174ebdd273bce6e
Charlike Mike Reagent <olsten.larck@gmail.com>
2017-11-14 09:40:15 +0200

    fix(lint): run eslint and prettier
    Signed-off-by: Charlike Mike Reagent <olsten.larck@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,69 +3,69 @@
  * @license Apache-2.0
  */
 
-const path = require('path');
-const isCI = require('is-ci');
-const semver = require('semver');
-const gitLog = require('parse-git-log');
-const packageJson = require('package-json');
-const detectChange = require('detect-next-version');
+const path = require('path')
+const isCI = require('is-ci')
+const semver = require('semver')
+const gitLog = require('parse-git-log')
+const packageJson = require('package-json')
+const detectChange = require('detect-next-version')
 
-const format = 'prettier-eslint --write **/*.{mjs,js,jsx,es,es6}';
-const lint = 'eslint --format codeframe **/*.{mjs,js,jsx,es,es6} --fix';
-const style = ['yarn hela format', 'yarn hela lint'];
+const format = 'prettier-eslint --write **/*.{mjs,js,jsx,es,es6}'
+const lint = 'eslint --format codeframe **/*.{mjs,js,jsx,es,es6} --fix'
+const style = ['yarn hela format', 'yarn hela lint']
 const test = [
   'nyc --reporter=lcov node test/index.js',
   'nyc report',
   'nyc check-coverage',
-];
+]
 
-const precommit = ['yarn hela style', 'git status --porcelain', 'yarn hela test'];
-const commit = ['yarn hela ac gen', 'git add --all', 'gitcommit -s -S'];
+const precommit = ['yarn hela style', 'git status --porcelain', 'yarn hela test']
+const commit = ['yarn hela ac gen', 'git add --all', 'gitcommit -s -S']
 
 /* eslint-disable no-shadow */
 
 const release = async ({ helaShell }) => {
-  const commits = await gitLog.promise();
-  const lastCommit = commits[0];
-  const commit = detectChange(lastCommit.contents, true);
-  console.log(commit);
-  if (!commit.increment) return null;
+  const commits = await gitLog.promise()
+  const lastCommit = commits[0]
+  const commit = detectChange(lastCommit.contents, true)
+  console.log(commit)
+  if (!commit.increment) return null
 
-  const pkgName = path.basename(lastCommit.cwd);
-  const pkgJson = await packageJson(pkgName);
-  const currentVersion = pkgJson.version;
-  const nextVersion = semver.inc(currentVersion, commit.increment);
+  const pkgName = path.basename(lastCommit.cwd)
+  const pkgJson = await packageJson(pkgName)
+  const currentVersion = pkgJson.version
+  const nextVersion = semver.inc(currentVersion, commit.increment)
 
   return helaShell([
     `yarn version --no-git-tag-version --new-version ${nextVersion}`,
     `${path.join(__dirname, 'publisher.sh')}`,
-  ]);
-};
+  ])
+}
 
 const protect = () => {
   /* istanbul ignore next */
   if (isCI) {
-    return Promise.resolve();
+    return Promise.resolve()
   }
 
-  const msg = 'the "npm publish" is forbidden, we use semantic-release on CI';
+  const msg = 'the "npm publish" is forbidden, we use semantic-release on CI'
 
-  return Promise.reject(new Error(msg));
-};
+  return Promise.reject(new Error(msg))
+}
 
 const ac = ({ argv, helaExec }) => {
-  const arg = argv._.slice(1).shift();
+  const arg = argv._.slice(1).shift()
   if (!arg) {
-    return helaExec('all-contributors init');
+    return helaExec('all-contributors init')
   }
   if (arg === 'a' || arg === 'add') {
-    return helaExec('all-contributors add');
+    return helaExec('all-contributors add')
   }
   if (arg === 'g' || arg === 'gen' || arg === 'generate') {
-    return helaExec('all-contributors generate');
+    return helaExec('all-contributors generate')
   }
-  return Promise.reject(new Error('hela ac: provide "add" or "gen" command'));
-};
+  return Promise.reject(new Error('hela ac: provide "add" or "gen" command'))
+}
 
 module.exports = {
   ac,
@@ -77,4 +77,4 @@ module.exports = {
   commit,
   release,
   protect,
-};
+}
